fed543e3f301b2b41b31598c5a97ed7e0631cbfd
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2021-06-27 16:39:28 +0400

    Minified a JS to One Line

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,1 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-    if(this.scrollY > 20){
-      $(".navbar").addClass("sticky");
-      $(".goTop").fadeIn();
-    } else{
-      $(".navbar").removeClass("sticky");
-      $(".goTop").fadeOut();
-    }
-  });
-  $(".goTop").click(function(){$('html, body').animate({scrollTop:0}, 300)});
-  $('.menu-toggler').click(function(){
-    $(this).toggleClass("active");
-    $(".navbar-menu").toggleClass("active");
-  });
-  document.getElementById("yearCount").innerHTML = new Date().getFullYear()
-});
+$(document).ready(function(){$(window).scroll(function(){20<this.scrollY?($(".navbar").addClass("sticky"),$(".goTop").fadeIn()):($(".navbar").removeClass("sticky"),$(".goTop").fadeOut())}),$(".goTop").click(function(){$("html, body").animate({scrollTop:0},300)}),$(".menu-toggler").click(function(){$(this).toggleClass("active"),$(".navbar-menu").toggleClass("active")}),document.getElementById("yearCount").innerHTML=(new Date).getFullYear()});
