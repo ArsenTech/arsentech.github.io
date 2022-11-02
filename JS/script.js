bdbcdc542227ef564da30f48c9fb0e7bfabbcac0
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2022-11-02 21:04:58 +0400

    Changed from let to const

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
-let gotop = document.querySelector(".goTop"),
+const gotop = document.querySelector(".goTop"),
      navbar = document.querySelector(".navbar"),
      toggler = document.querySelector(".menu-toggler"),
      navMenu = document.querySelector(".navbar-menu");
 function lazyCss(e) {
-     let t = document.createElement( "link" );
+     const t = document.createElement( "link" );
      t.href = e, t.rel = "stylesheet", t.type = "text/css", document.getElementsByTagName("head")[0].appendChild(t)
 }
 lazyCss("https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;700&display=swap")
