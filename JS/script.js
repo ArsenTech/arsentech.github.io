b6e7d61fdbd9ec91f48b4822ec8650df528bfa00
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2022-11-29 20:36:41 +0400

    Made One JS File Load Lazy

--- JS/script.js
+++ JS/script.js
@@ -6,8 +6,13 @@ function lazyCss(e) {
      const t = document.createElement( "link" );
      t.href = e, t.rel = "stylesheet", t.type = "text/css", document.getElementsByTagName("head")[0].appendChild(t)
 }
+function lazyJS(e){
+     const t = document.createElement("script");
+     t.src = e, document.body.appendChild(t)
+}
 lazyCss("https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;700&display=swap")
 lazyCss("CSS/lightbox.min.css")
+lazyJS("JS/lightbox-plus-jquery.min.js")
 document.getElementById("yearCount").innerHTML = (new Date).getFullYear()
 window.addEventListener("scroll", () => {20 < this.scrollY ? navbar.classList.add("sticky") : navbar.classList.remove("sticky"); window.pageYOffset > 100 ? gotop.classList.add("active") : gotop.classList.remove("active")})
 gotop.addEventListener("click", () => window.scrollTo({top: 0, left: 0,behavior: "smooth"}))
