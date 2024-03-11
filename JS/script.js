1fb9b048822c06fd140228e715592b57c27855cc
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2024-03-11 22:07:37 +0400

    Added Subscribe Channel Popup

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,8 @@
 const gotop = document.querySelector(".goTop"),
 navbar = document.querySelector(".navbar"),navMenu = document.querySelector(".navbar-menu"),
 toggler = document.querySelector(".menu-toggler"),modeToggler = document.querySelector("#icon"),
-getMode = localStorage.getItem("arsentech-theme");
+getMode = localStorage.getItem("arsentech-theme"),subClose = document.querySelector('#sub-close'),
+subPopup = document.querySelector('.subscribe');
 if(getMode && getMode === "dark") {document.body.classList.add("dark");modeToggler.querySelector("img").src = "Files/icons/dark.svg";lazyCss("CSS/dark-mode.css")}
 window.addEventListener("scroll", ()=>handleScroll(20,100));
 gotop.addEventListener("click", ()=>window.scrollTo({top: 0, left: 0,behavior: "smooth"}));
@@ -15,4 +16,5 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e =
           localStorage.setItem("arsentech-theme", "light");removeCss();
      }
 });
+subClose.addEventListener('click',()=>subPopup.classList.add('hide'))
 lazyCss("https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;700&display=swap");initCards();
