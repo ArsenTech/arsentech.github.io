f56a57cb24b86268370214d8c8e3f7233423663a
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2024-12-15 20:18:26 +0400

    Added Patreon Link, Changes in About Section, Made The Second Button To a Patreon Link In Hero Section, Changed The Image Of 1 Work To Fit Perfectly, Changes In serviceData, worksData, and skillData

--- JS/functions.js
+++ JS/functions.js
@@ -1,26 +1,26 @@
 const skillData = [
      {name:"HTML",class:"html",percentage:90},
-     {name:"CSS",class:"css",percentage:90},
+     {name:"CSS, Sass (SCSS)",class:"css",percentage:90},
      {name:"Javascript",class:"js",percentage:90},
      {name:"React JS",class:"react",percentage:90},
-     {name:"SASS (SCSS)",class:"scss",percentage:90},
+     {name:"Next JS",class:"nextjs",percentage:90},
      {name:"Angular",class:"angular",percentage:75}
 ]
 const serviceData = [
      {name:"Tutorials",desc:"This is Mostly important for All Windows, iOS, And Linux Users (Check in YouTube)",icon:"ant-design:laptop-outlined",link:"https://www.youtube.com/playlist?list=PLsOMVP9OgGeosVdpYLlVuNTa-iiOSvGHS"},
-     {name:"Coding with ArsenTech",desc:"Coding Tutorials with HTML, CSS, JS, SCSS, Bootstrap, React.JS",icon:"akar-icons:youtube-fill",link:"https://www.youtube.com/channel/UCl52C6cFR1McvN1fAdsxdkA"},
+     {name:"Coding with ArsenTech",desc:"Coding Tutorials to become a Skilled Programmer",icon:"bx:bx-code-alt",link:"https://www.youtube.com/channel/UCl52C6cFR1McvN1fAdsxdkA"},
      {name:"Games",desc:"Games Made on Scratch by ArsenTech. (These Are Basic / Arcade Games)",icon:"ion:game-controller",link:"https://scratch.mit.edu/users/ArsenTech/"},
-     {name:"Coding",desc:"Check My Github Repositories for Web Development",icon:"bx:bx-code-alt",link:"https://github.com/ArsenTech?tab=repositories"},
+     {name:"Windows Experiments",desc:"Windows Test Videos Made By ArsenTech (Educational Purposes Only)",icon:"ion:hammer",link:"https://scratch.mit.edu/users/ArsenTech/"},
      {name:"Downloads",desc:"Download Windows Versions, Softwares and Some Files from My Collection",icon:"fa:download",link:"https://arsentech.github.io/downloads/"},
-     {name:"Malware Testing",desc:"Malware Testing Content and Antivirus VS Malware Videos",icon:"fa-solid:bug",link:"https://youtube.com/playlist?list=PLsOMVP9OgGeoAFJZdOofZQS3R0g5G57ot"}
+     {name:"Malware Testing",desc:"Malware Testing Content and Antivirus Test Videos",icon:"fa-solid:bug",link:"https://youtube.com/playlist?list=PLsOMVP9OgGeoAFJZdOofZQS3R0g5G57ot"}
 ];
 const worksData = [
-     {img:"Files/Work/code.webp",name:"Programming",category:"Development, Programming",attr:"Photo: Pixabay from Pexels",imgAlt:"coding"},
-     {img:"Files/Work/wintuto.webp",name:"Tutorials",category:"Windows, Linux",attr:"Photo: Max DeRoin from Pexels",imgAlt:"keyboard"},
-     {img:"Files/Work/lintuto.webp",name:"Windows Experiments",category:"Windows 10, Windows 7",attr:"Photo: Negative Space from Pexels",imgAlt:"computer"},
-     {img:"Files/Work/downloads.webp",name:"Downloads",category:"Software, Wallpaper",attr:"Photo: Miguel Á. Padriñán from Pexels",imgAlt:"downloads"},
-     {img:"Files/Work/virustest.webp",name:"Malware Testing",category:"Virus Test, Malware Test",attr:"Photo: Markus Spiske from Pexels",imgAlt:"matrix"},
-     {img:"Files/Work/ios.webp",name:"ArsenTech Shorts",category:"Quick Tutorials, Tips and Tricks",attr:"Photo: Tracy Le Blanc from Pexels",imgAlt:"phone"},
+     {img:"Files/Work/code.webp",name:"Programming",category:"Development, Programming",attr:"Photo by luis  from Pexels",imgAlt:"coding"},
+     {img:"Files/Work/wintuto.webp",name:"Tutorials",category:"Windows, Linux",attr:"Photo by Max DeRoin from Pexels",imgAlt:"keyboard"},
+     {img:"Files/Work/lintuto.webp",name:"Windows Experiments",category:"Windows 10, Windows 7",attr:"Photo by Negative Space from Pexels",imgAlt:"computer"},
+     {img:"Files/Work/downloads.webp",name:"Downloads",category:"Software, Wallpaper",attr:"Photo by Miguel Á. Padriñán from Pexels",imgAlt:"downloads"},
+     {img:"Files/Work/virustest.webp",name:"Malware Testing",category:"Virus Test, Malware Test",attr:"Photo by Markus Spiske from Pexels",imgAlt:"matrix"},
+     {img:"Files/Work/ios.webp",name:"ArsenTech Shorts",category:"Quick Tutorials, Tips and Tricks",attr:"Photo by Tracy Le Blanc from Pexels",imgAlt:"phone"},
 ]
 const removeCss = ()=>document.querySelector("link[href='CSS/dark-mode.css']").remove();
 function lazyCss(e) {const t = document.createElement( "link" );t.href = e, t.rel = "stylesheet", t.type = "text/css", t.media="screen", document.getElementsByTagName("head")[0].appendChild(t);}
